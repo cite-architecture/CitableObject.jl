6b6150b2cb59d6b78aeabee1d66d54d5e6c7755b
Neel Smith <nsmith@Technites.local>
2021-01-01 18:17:23 -0500

    Test URN validation

--- test/urn_validation.jl
+++ test/urn_validation.jl
@@ -1,4 +1,14 @@
 
 @testset "URN validation" begin
+    @test_throws ArgumentError("Invalid string `way:too:short`: Cite2Urns must have five top-level components.") Cite2Urn("way:too:short")
     @test_throws ArgumentError("Invalid string `this:shouldn't:work:at:all!`: URNs must begin with `urn:`.") Cite2Urn("this:shouldn't:work:at:all!")
+    @test_throws ArgumentError("Invalid string `urnX:cite2:hmt:msA.v1:12r`: URNs must begin with `urn:`.") Cite2Urn("urnX:cite2:hmt:msA.v1:12r")
+    @test_throws ArgumentError("Invalid string `urn:cite2X:hmt:msA.v1:12r`: the URN type must be `cite2`.") Cite2Urn("urn:cite2X:hmt:msA.v1:12r")
+    @test_throws ArgumentError("Invalid string `urn:cite2::msA.v1:12r`: CITE namespace cannot be empty.") Cite2Urn("urn:cite2::msA.v1:12r")
+    @test_throws ArgumentError("Invalid string `urn:cite2::msA.v1:12r`: CITE namespace cannot be empty.") Cite2Urn("urn:cite2::msA.v1:12r")
+    
+    # Ok for object component to be empty:
+    @test validurn("urn:cite2:hmt:msA.v1:")
+    @test validurn("urn:cite2:hmt:msA.v1:12r")
+   
 end
