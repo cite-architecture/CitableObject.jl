cd18a5322645c8af2d53c39ed255bf310c2ba5cc
Neel Smith <dnsmith.neel@gmail.com>
2022-02-21 07:59:30 -0500

    Fully implement optional URN API from `CitableBase` 10.2

--- src/CitableObject.jl
+++ src/CitableObject.jl
@@ -8,16 +8,18 @@ using CitableBase
 import CitableBase: supportsversion
 import CitableBase: dropversion
 import CitableBase: addversion
+import CitableBase: versionid
+
+import CitableBase: supportssubref
+import CitableBase: dropsubref
+import CitableBase: hassubref
+import CitableBase: subref
 
 import CitableBase: urncomparisontrait
 import CitableBase: urnequals
 import CitableBase: urncontains
 import CitableBase: urnsimilar
 
-import CitableBase: supportssubref
-import CitableBase: dropsubref
-import CitableBase: hassubref
-import CitableBase: subref
 export Cite2Urn
 export CitableByCite2Urn
 export Cite2UrnComparable
@@ -27,8 +29,6 @@ export collectionid, versionid, propertyid
 export dropobject, addobject
 export dropproperty, addproperty
 export isrange, rangebegin, rangeend
- 
-
 
 include("cite2urn.jl")
 include("validurn.jl")
