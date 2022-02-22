abded4228269586a16d4dba672269c1912816ce4
Neel Smith <dnsmith.neel@gmail.com>
2022-02-22 08:57:53 -0500

    Use raw string comparison on properties retrieval

--- src/cexutils/properties.jl
+++ src/cexutils/properties.jl
@@ -2,71 +2,67 @@ using CitableObject
 using CiteEXchange
 
 #=
-1. collect properties for urn
+1. √ collect properties for urn
 5.  collect data blocks for urn
 3. collect collection urns for datamodel:
 4. collect relation blocks for urn
 =#
 
-"""From a list of string values, find those with first delimited column
-matching `u` by URN containment.
+"""From a list of string values, extract those where the first delimited column
+matches `u` by URN containment.
 $(SIGNATURES)
+Since property definitions must be specific to a version, we can use string comparison
+rather than the more expensive `urncontains` to filter these values.
 """
-function matchingproperties(lines::Vector{<: AbstractString}, u::Cite2Urn, delimiter::AbstractString)
-    matchinglines = []
-    for prop in lines
-        columns = split(prop, delimiter)
-        if urncontains(u, Cite2Urn(columns[1]))
-            push!(matchinglines, prop)
-        end
-    end
-    matchinglines
+function matchingproperties(lines::Vector{<: AbstractString}, u::Cite2Urn)
+    collectionstr = string(u)[1:end-1] # drop trailing ":"
+    filter(ln -> startswith(ln, collectionstr), lines)
 end
 
 """Find all property definitions in the CEX string `s` contained by `u`.
 $(SIGNATURES)
 """
-function properties(s::AbstractString, u::Cite2Urn; delimiter = "|")
+function properties(s::AbstractString, u::Cite2Urn)
     propdata = data(s, "citeproperties")
-    matchingproperties(propdata,u,delimiter)
+    matchingproperties(propdata,u)
 end
 
 """Find all property definitions in `blks` contained by `u`.
 $(SIGNATURES)
 """
-function properties(blks::Vector{Block}, u::Cite2Urn; delimiter = "|")
+function properties(blks::Vector{Block}, u::Cite2Urn)
     propdata = data(blks, "citeproperties")
-    matchingproperties(propdata,u,delimiter)
+    matchingproperties(propdata,u)
 end
 
 """Dispatch `properties` on `T`.
 
 $(SIGNATURES)
 """    
-function properties(cexsrc::AbstractString, u::Cite2Urn, reader::T; delimiter = "|") where {T <: ReaderType}
-    properties(cexsrc, u, T, delimiter = delimiter)
+function properties(cexsrc::AbstractString, u::Cite2Urn, reader::T) where {T <: ReaderType}
+    properties(cexsrc, u, T)
 end
 
 """Find all property definitions in CEX file `filesrc` contained by `u`.
 $(SIGNATURES)
 """
-function properties(filesrc::AbstractString, u::Cite2Urn, freader::Type{FileReader}; delimiter = "|")
+function properties(filesrc::AbstractString, u::Cite2Urn, freader::Type{FileReader})
     s = read(filesrc) |> String
-    properties(s, u, delimiter = delimiter)
+    properties(s, u)
 end
 
 
 """Find all property definitions in CEX at URL `url` contained by `u`.
 $(SIGNATURES)
 """
-function properties(url::AbstractString, u::Cite2Urn, ureader::Type{UrlReader}; delimiter = "|")
+function properties(url::AbstractString, u::Cite2Urn, ureader::Type{UrlReader})
     s = Downloads.download(url) |> read |> String
-    properties(s, u, delimiter = delimiter)
+    properties(s, u)
 end
 
 """Find all property definitions in CEX at URL `url` contained by `u`.
 $(SIGNATURES)
 """
-function properties(s::AbstractString, u::Cite2Urn, sreader::Type{StringReader}; delimiter = "|")
-    properties(s, u, delimiter = delimiter)
+function properties(s::AbstractString, u::Cite2Urn, sreader::Type{StringReader})
+    properties(s, u)
 end
