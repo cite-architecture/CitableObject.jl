ce2b3a62d3bee48e0d155b2da7098fc363578df6
Neel Smith <dnsmith.neel@gmail.com>
2022-02-24 16:14:09 -0500

    Test `citeids` function

--- test/test_cexlabels.jl
+++ test/test_cexlabels.jl
@@ -14,7 +14,6 @@
     
 end
 
-
 @testset "Test retrieving labels for relation sets" begin
     f = joinpath(pwd(), "data", "hmt-2022k.cex")
     s = read(f) |> String
@@ -28,5 +27,33 @@ end
     @test relationsetlabel(f, dseurn, FileReader) == expected
     @test relationsetlabel(u, dseurn, UrlReader) == expected
     @test relationsetlabel(s, dseurn, StringReader) == expected
+
+end
+
+
+
+@testset "Test retrieving labels for implementations of a data model" begin
+    f = joinpath(pwd(), "data", "hmt-2022k.cex")
+    s = read(f) |> String
+    u = "https://raw.githubusercontent.com/cite-architecture/CitableObject.jl/main/test/data/hmt-2022k.cex" 
+
+    codmod = Cite2Urn("urn:cite2:hmt:datamodels.v1:codexmodel")
+    expectedcodices =  7
+    codexids = citeids(s, codmod)
+    @test length(codexids) == expectedcodices
+
+    c1 = codexids[1]
+    @test c1.urn == Cite2Urn("urn:cite2:citebl:burney86pages.v1:")
+    @test c1.label == "Burney 86 manuscript"
+
+
+    @test codexids == citeids(blocks(s), codmod)
+    @test codexids == citeids(f, codmod, FileReader)
+    @test codexids == citeids(u, codmod, UrlReader)
+    @test codexids == citeids(s, codmod, StringReader)
+    
+    # Relation set model:
+    txtonpage = Cite2Urn("urn:cite2:hmt:datamodels.v1:textonpage") 
+
 
 end
